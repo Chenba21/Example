c9f89e7013627e085443f0c55b37bc942635e787
Chenbagaam S <schenbagaam21@gmail.com>
2023-10-18 14:49:25 +0530

    Initial commit

---
+++ Pratical/src/Loop.java
@@ -0,0 +1,12 @@
+public class Loop 
+{
+	//Loops are repetitive
+	//for..while..do..while
+	public static void main(String args[])
+	{
+		for(int i=10;i<=20;i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
